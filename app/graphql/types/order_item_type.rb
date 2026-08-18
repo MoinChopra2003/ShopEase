1aa6b769b18e60dc96c572c77259b1b8a1365e83
Abdullah Johar <your-email@example.com>
2026-08-18 15:08:47 +0500

    fix GraphQL order item price

--- app/graphql/types/order_item_type.rb
+++ app/graphql/types/order_item_type.rb
@@ -4,5 +4,9 @@ module Types
     field :product, Types::ProductType, null: false
     field :quantity, Int, null: false
     field :price, Float, null: false
+
+    def price
+      object.price_at_purchase
+    end
   end
 end
